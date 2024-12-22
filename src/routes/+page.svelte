5c48270c5e338d4097571dc90255cc361ca45be6
Andy Robinson <andy@opentrack.run>
2024-12-22 23:52:18 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let title = "Robinson new website";
+
+</script>
+<h1>{title}!</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>Design goals:</p>
+<ul>
+	<li>Keep existing images and directories</li>
+	<li>Break galleries into tabs / groups</li>
+	<li>Allow annotations on images</li>
+	<li>Optionally order images</li>
+	<li>Control with a YAML or similar file in each folder</li>
+</ul>
+
+<p>Useful tutorials...</p>
+<a href="https://www.okupter.com/blog/deploy-sveltekit-website-to-github-pages">Github Pages</a>
